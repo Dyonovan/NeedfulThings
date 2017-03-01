43bae3e82058b0e33507cbb307b4bd99c104874d
Dyonovan <lookinforfun0000@gmail.com>
2017-02-28 21:44:09 -0500

    Added player head drops

--- src/main/java/com/dyonovan/needfulthings/enchantments/EnchantmentBeheading.java
+++ src/main/java/com/dyonovan/needfulthings/enchantments/EnchantmentBeheading.java
@@ -5,14 +5,19 @@ import crazypants.enderio.machine.enchanter.EnchanterRecipeManager;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnumEnchantmentType;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.monster.*;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.fml.common.Loader;
@@ -55,7 +60,7 @@ public class EnchantmentBeheading extends Enchantment {
     }
 
     private EnchantmentBeheading() {
-        super(Rarity.VERY_RARE, EnumEnchantmentType.WEAPON, new EntityEquipmentSlot[]{EntityEquipmentSlot.MAINHAND, EntityEquipmentSlot.OFFHAND});
+        super(Rarity.RARE, EnumEnchantmentType.WEAPON, new EntityEquipmentSlot[]{EntityEquipmentSlot.MAINHAND, EntityEquipmentSlot.OFFHAND});
         setName(NAME);
         setRegistryName(NAME);
     }
@@ -67,7 +72,7 @@ public class EnchantmentBeheading extends Enchantment {
 
     @Override
     public int getMinEnchantability(int level) {
-        return 16;
+        return 16 * level;
     }
 
     @Override
@@ -78,17 +83,40 @@ public class EnchantmentBeheading extends Enchantment {
     @Override
     public boolean canApplyTogether(Enchantment ench) { return true; }
 
+    @Override
+    public boolean isAllowedOnBooks() {
+        return true;
+    }
+
     //Capture event for Drops
     @SubscribeEvent
     public void onLivingDropsEvent(LivingDropsEvent event) {
         DamageSource source = event.getSource();
-        if (source.getDamageType().equalsIgnoreCase("player")) {
-            EntityPlayer player = (EntityPlayer) event.getSource().getSourceOfDamage();
-            int level = EnchantmentHelper.getEnchantmentLevel(Enchantment.getEnchantmentByLocation(Reference.MOD_ID + ":" + NAME), player.getHeldItemMainhand());
-            if (level > 0) {
-                ItemStack skull = getSkullForEntity((EntityLivingBase) event.getEntity());
-                if (skull != null) {
-                    event.getDrops().add(new EntityItem(player.getEntityWorld(), player.posX, player.posY, player.posZ, skull));
+        if (source.getDamageType().equalsIgnoreCase("player") || source.getDamageType().equalsIgnoreCase("arrow")) {
+            EntityPlayer player = null;
+            if (source.getDamageType().equalsIgnoreCase("arrow")) {
+                if (((EntityArrow) event.getSource().getSourceOfDamage()).shootingEntity instanceof EntityPlayer)
+                    player = (EntityPlayer) ((EntityArrow) event.getSource().getSourceOfDamage()).shootingEntity;
+            } else
+                player = (EntityPlayer) event.getSource().getSourceOfDamage();
+            if (player != null) {
+                int level = EnchantmentHelper.getEnchantmentLevel(Enchantment.getEnchantmentByLocation(Reference.MOD_ID + ":" + NAME), player.getHeldItemMainhand());
+                if (level > 0) {
+                    ItemStack skull = getSkullForEntity((EntityLivingBase) event.getEntity());
+                    if (skull != null) {
+                        int random = (MathHelper.getRandomIntegerInRange(player.getRNG(), 0, 100) + (level * 10));
+                        //System.out.println(random);
+                        if (random >= 80 || skull.getItemDamage() == 3) {
+                            if (!event.getDrops().contains(skull)) {
+                                World world = player.getEntityWorld();
+                                Entity entity = event.getEntity();
+                                float rx = world.rand.nextFloat() * 0.8F;
+                                float ry = world.rand.nextFloat() * 0.8F;
+                                float rz = world.rand.nextFloat() * 0.8F;
+                                event.getDrops().add(new EntityItem(world, entity.posX + rx, entity.posY + ry, entity.posZ + rz, skull));
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -108,6 +136,12 @@ public class EnchantmentBeheading extends Enchantment {
             return new ItemStack(Items.SKULL, 1, 4);
         } else if (entityLiving instanceof EntityEnderman && Loader.isModLoaded("EnderIO")) {
                 return enderioEnderSkull();
+        } else if (entityLiving instanceof EntityPlayer) {
+            NBTTagCompound tag = new NBTTagCompound();
+            tag.setString("SkullOwner", entityLiving.getName());
+            ItemStack stack = new ItemStack(Items.SKULL, 1, 3);
+            stack.setTagCompound(tag);
+            return stack;
         }
         return null;
     }
